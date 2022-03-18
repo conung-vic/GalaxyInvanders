60e7fe3e64d88085fba7c014cb5dc2c91e44663c
Igor <conung.vic@gmail.com>
2022-03-18 20:46:49 +0200

    fix bug if username is not latin

--- desktop/src/com/conungvic/gigame/desktop/DesktopLauncher.kt
+++ desktop/src/com/conungvic/gigame/desktop/DesktopLauncher.kt
@@ -5,6 +5,8 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration
 import com.conungvic.gigame.GIGame
 import com.conungvic.gigame.V_HEIGHT
 import com.conungvic.gigame.V_WIDTH
+import java.nio.charset.Charset
+import java.nio.charset.StandardCharsets
 
 fun main(args: Array<String>) {
     val config = LwjglApplicationConfiguration()
@@ -15,6 +17,28 @@ fun main(args: Array<String>) {
     config.foregroundFPS = 120
     config.backgroundFPS = 120
     config.resizable = true
+    checkIsUsernameNotLatin()
 //    config.fullscreen = true
     LwjglApplication(GIGame(), config)
+}
+
+fun checkIsUsernameNotLatin() {
+    val name = System.getProperty("user.name")
+    if (!isPureAscii(name)) {
+        val buf = StandardCharsets.UTF_8.encode(name)
+        val sBuf = StringBuffer()
+        for (b in buf.array()) {
+            if (1*b != 0) {
+                val hex = Integer.toHexString(1 * b).substring(6, 8).uppercase()
+                sBuf.append("\\x$hex")
+            }
+        }
+        System.setProperty("user.name", sBuf.toString())
+    }
+}
+
+fun isPureAscii(v: String): Boolean {
+    return Charset.forName(StandardCharsets.ISO_8859_1.name()).newEncoder().canEncode(v)
+    // or "ISO-8859-1" for ISO Latin 1
+    // or StandardCharsets.US_ASCII with JDK1.7+
 }
